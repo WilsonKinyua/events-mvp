d90108c4d0ba346b66f8e108145b21117714477f
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-02 11:36:13 +0300

    displaying of companies done

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Company;
 use App\Models\EventSetting;
 
 class HomeController
@@ -9,6 +10,7 @@ class HomeController
     public function index()
     {
         $eventSetting = EventSetting::with(['media'])->get()->first();
-        return view('home', compact('eventSetting'));
+        $companies = Company::with(['media'])->get();
+        return view('home', compact('eventSetting','companies'));
     }
 }
